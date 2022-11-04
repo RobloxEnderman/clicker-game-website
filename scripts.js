80fcee0eb586371f5dd59b15202e8e79c8a8d95a
Matthew Yak <matthew@matthewyak.com>
2022-11-04 14:04:04 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,10 +10,12 @@ function Click(){
 
 function Upgrade() {
     if (balance >= (10*multiplier)){
-        multiplier += 0.5 * multiplier;
-        multiplier = (Math.round(multiplier * 100)/100)
+        var multiplierCostMultiplier = 1;
+        multiplier += 0.5 * multiplierCostMultiplier;
         balance -= (10*multiplier);
-        multiplierCost = (Math.round((10*multiplier)*100))/100
+        multiplierCost = (10*multiplier);
+        multiplierCostMultiplier += 1;
+        
         updateCount()
         document.getElementById('Multiplier').innerText = 'Upgrade Multiplier ($'+(multiplierCost)+ ')'
     }
