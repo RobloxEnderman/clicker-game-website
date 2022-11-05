55bf6ab9e8ac0fe6d1c6a5dc6459970f942ea33c
Matthew Yak <matthew@matthewyak.com>
2022-11-04 20:10:32 -0700

    Organized scripts

--- scripts.js
+++ scripts.js
@@ -7,27 +7,8 @@ let multiplierCostMultiplier = 1;
 document.getElementById('Multiplier').innerText = 'Upgrade Multiplier ($' + (multiplierCost) + ')'
 
 
-function Click() {
-  balance += (1 * multiplier);
-  updateCount();
-  document.getElementById('collectmoney').hidden = true;
-  document.getElementById('collectmoney').hidden = false;
-}
 
 
-
-function Upgrade() {
-  if (balance >= (multiplierCost * multiplierCostMultiplier)) {
-
-    multiplier += 0.5 * multiplierCostMultiplier;
-    balance -= (multiplierCost * multiplierCostMultiplier);
-
-    multiplierCostMultiplier += 1;
-    multiplierCost = (multiplierCost * multiplierCostMultiplier);
-    updateCount()
-    document.getElementById('Multiplier').innerText = 'Upgrade Multiplier ($' + (multiplierCost * multiplierCostMultiplier) + ')'
-  }
-}
 
 function updateCount() {
   document.getElementById('Text').innerText = '$' + balance;
